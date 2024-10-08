5deb5c403005f4111eee26dbc07a72f898ae1f95
xing24xing <khushipandey260304@gmail.com>
2024-10-08 17:47:16 +0530

    Solve err 2.0

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors({
-    origin: 'https://twinkkletalk.netlify.app',
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 app.use(cookieParser());
